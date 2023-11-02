d7b67240ad18452ff3d0a4b0ecb2f61090a887f7
jmarqb <jmarqbvpn@gmail.com>
2023-11-02 12:28:32 -0500

    config dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -27,5 +28,6 @@ async function bootstrap() {
   SwaggerModule.setup('api/doc', app, document);
 
   await app.listen(process.env.PORT);
+  console.log('APP running in port:',process.env.PORT);
 }
 bootstrap();
